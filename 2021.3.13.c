222c8b973254f442e3d4206026aecee08f542d84
chloe <3542346746@qq.com>
2021-03-29 21:10:21 +0800

    和 常量 枚举

--- 2021.3.13.c
+++ 2021.3.13.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-//int main()
-//{
-//	int num1 = 0;
-//	int num2 = 0;
-//	int sum = 0;
-//	scanf_s("%d%d", &num1, &num2);
-//	sum = num1 + num2;
-//	printf("sum = %d\n", sum);
-//
-//	return 0;
-//}
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	scanf_s("%d%d", &num1, &num2);
+	sum = num1 + num2;
+	printf("sum = %d\n", sum);
+
+	return 0;
+}
 
 
 
@@ -24,29 +24,29 @@
 
 
 // #define 定义的标识符常量
-//#define MAX 10
-//
-//int main()
-//{
-//	int arr[MAX] = { 0 };
-//	printf("%d\n", MAX);
-//	return 0;
-//}
+#define max 10
+
+int main()
+{
+	int arr[max] = { 0 };
+	printf("%d\n", max);
+	return 0;
+}
 
 
 // 枚举常量
 // 枚举关键 enum
-//enum Sex
-//{
-//	MALE,  // 枚举常量
-//	FEMALE,  // 枚举常量
-//	SECRET  // 枚举常量
-//};
-//int main()
-//{
-//	printf("%d\n", MALE);  // 0
-//	printf("%d\n", FEMALE);  // 1
-//	printf("%d\n", SECRET);  // 2
-//
-//	return 0;
-//}
+enum Sex
+{
+	MALE,  // 枚举常量
+	FEMALE,  // 枚举常量
+	SECRET  // 枚举常量
+};
+int main()
+{
+	printf("%d\n", MALE);  // 0
+	printf("%d\n", FEMALE);  // 1
+	printf("%d\n", SECRET);  // 2
+
+	return 0;
+}
