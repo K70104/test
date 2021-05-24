5824166e487fede94b655e1ae938628b2ab96c21
chloe <3542346746@qq.com>
2021-05-24 12:53:32 +0800

    数组的存储

--- test_5_17.c
+++ test_5_17.c
@@ -207,3 +207,19 @@ int main()
 	printf("%If\n", d); // 1.0*10^10  1.0乘10的10次方
 	return 0;
 }
+
+
+#include <stdio.h>
+
+int main()
+{
+	int n = 9;
+	float* pFloat = (float*)&n;
+	printf("n的值为：%d\n", n);
+	printf("pFloat的值为：%f\n", *pFloat);
+
+	*pFloat = 9.0;
+	printf("num的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+	return 0;
+}
